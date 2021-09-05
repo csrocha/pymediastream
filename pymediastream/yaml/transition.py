89f2f66cad16d5cdaeced12e4f7058035b1774c2
Cristian S. Rocha <cristian.rocha@moldeo.coop>
2021-09-05 09:02:15 -0300

    Add reset to null elements in transitions.

--- pymediastream/yaml/transition.py
+++ pymediastream/yaml/transition.py
@@ -14,12 +14,22 @@ class Transition(YAMLObject):
         self._final_state = final_state
 
     def run(self, pipeline):
-        prev_state = pipeline._pipeline.current_state
-        pipeline.set_state(Gst.State.NULL)
+        prev_state = {}
         for element_name, setup in self._final_state.items():
+            if '_null_reset' in setup:
+                element = pipeline.get(element_name)
+                prev_state[element_name] = element.current_state
+                element.set_state(Gst.State.NULL)
+
+        for element_name, setup in self._final_state.items():
+            to_setup = {k:v for k, v in setup.items() if k[0] != '_'}
+            if to_setup:
+                element = pipeline.get(element_name)
+                element.set_properties(**to_setup)
+
+        for element_name, next_state in prev_state.items():
             element = pipeline.get(element_name)
-            element.set_properties(**setup)
-        pipeline.set_state(prev_state)
+            element.set_state(next_state)
 
     @classmethod
     def from_yaml(cls, loader, node):
